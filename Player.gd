ffe512149e748f12f98ef57b17cd9db3dabd8c61
PeppaFan666 <treebarker2007@gmail.com>
2022-07-15 15:53:42 -0600

    player can shoot
    was suprisingly easy to implement

--- Player.gd
+++ Player.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 export var speed = 200
 export var friction = 0.01
 export var acceleration = 0.1
+export var timer_cooldown = 0.1
+export var shot_duration = 0.1
+
+var canshoot = true
 
 onready var parts = get_parent().get_node("Viewport/Particles2D")
 
@@ -25,7 +29,20 @@ func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	rotation_degrees += 90
 
+func _process(delta):
+	parts.process_material.set_shader_param("spawn_angle_base",rotation - deg2rad(90))
+
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.pressed:
+		if event.pressed and canshoot:
 			parts.emitting = true
+			$Timer.start(shot_duration)
+
+
+func _on_Timer_timeout():
+	parts.emitting = false
+	$Timer2.start(timer_cooldown)
+
+
+func _on_Timer2_timeout():
+	canshoot = true
