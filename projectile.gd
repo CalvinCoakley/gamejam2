1ead465efd95365bd2653ee7c7fcd3c5b41cdc8b
PeppaFan666 <treebarker2007@gmail.com>
2022-07-17 13:15:45 -0600

    more stuff

--- projectile.gd
+++ projectile.gd
@@ -16,8 +16,8 @@ func _on_KinematicBody2D_body_entered(body):
 	if player_target:
 		if body.name == "Player":
 			Global.player_health -= 1
-			body.position -= velocity
+			#body.position -= velocity
 	elif body.is_in_group("Enemies"):
 		body.health -= 1
-		body.position -= velocity
+		#body.position -= velocity
 
