bb4c8d491c30914c698fffc02adae707a2df5df5
CalvinCoakley <calvin.coakley@gmail.com>
2022-07-17 15:04:00 -0400

    removal

--- delta!.gd
+++
@@ -1,5 +0,0 @@
-tool
-extends ColorRect
-
-func _process(delta):
-	material.set_shader_param("DELTA",delta)
--- title screen.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _on_Button_Pressed():
-	get_tree().change_scene("res://blue.tscn")
-
